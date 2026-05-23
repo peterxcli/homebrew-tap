27a925a38254e6e80529da6e13dfd1d32828235b
peterxcli <peterxcli@gmail.com>
2026-05-23 18:28:48 +0800

    Rename tap to ccost

--- Casks/ccost.rb
+++ Casks/ccost.rb
@@ -5,11 +5,10 @@ cask "ccost" do
   sha256 arm: "a4100904ec14280bf16b3031a5a07db64214096205a10bb27db890f294ae14f6",
          intel: "589be6cf80ee5163aedba13ecf0392e29c6c2c1aeec35ba4851427cdaa88c90f"
 
-  url "https://github.com/peterxcli/homebrew-tap/releases/download/ccost-v#{version}/ccost-#{arch}-apple-darwin.tar.gz"
+  url "https://github.com/peterxcli/homebrew-ccost/releases/download/ccost-v#{version}/ccost-#{arch}-apple-darwin.tar.gz"
   name "ccost"
   desc "Terminal UI for inspecting Codex session token usage and cost estimates"
-  homepage "https://github.com/peterxcli/codex-cost"
+  homepage "https://github.com/peterxcli/ccost"
 
   binary "ccost"
-  binary "codex-cost"
 end
